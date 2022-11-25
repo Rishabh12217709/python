5ae0fe95899b442440d7e9e3dcca524ff2f40fed
Rishabh12217709 <118722241+Rishabh12217709@users.noreply.github.com>
2022-11-25 09:24:49 +0530

    Add files via upload

---
+++ pythonproject.py
@@ -0,0 +1,15 @@
+<html>
+<title>Flipkart
+</title>
+<head>
+</head>
+<body>
+<table border="0                                     " width="100%" bgcolor="#2874F0">
+<tr>
+<td align="right"><img src="https://th.bing.com/th/id/R.004062578e2711cff426c36e31adca9c?rik=fQ3tbmXlw5c6pQ&riu=http%3a%2f%2fwww.tandonamit.com%2fimages%2fclient3.png&ehk=5o3PmXd%2f7s0ErUA6KLx%2f15vo9O4syNMJC8Cdcb0FnBU%3d&risl=&pid=ImgRaw&r=0"></td>
+<td><input type="text" placeholder="search for products,brands and more"></td>
+<th><font color="white">login & signup</font> </th>
+<th><font color="white">More</font></th>
+<td><font color="white">My Cart</font></td>
+</body>
+</html>
